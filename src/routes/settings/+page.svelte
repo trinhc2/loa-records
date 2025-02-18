a5ac3231b5030c4238f8e25b1770d93bff8f5e15
Christian Trinh <trinhc2@mcmaster.ca>
2025-02-18 16:15:42 -0500

    fixed stores, performs checks before reading settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { invoke } from '@tauri-apps/api/core';
+	import { logsFolder } from '$lib/stores'
 
 	interface Settings {
-		logsFileLocation: String;
+		logsFolderLocation: string;
 	}
 
 	let inputText: string = '';
 
 	async function createSettingsFile() {
 		try {
+			logsFolder.set(inputText)
 			const result = await invoke('create_settings_file', { folder: inputText });
 			console.log(result); // Log the result, or you can display a success message
+
 		} catch (error) {
 			console.error('Error creating settings file:', error);
 		}
@@ -19,15 +22,21 @@
 
 	async function getSettings() {
 		try {
-			const result = await invoke('read_settings');
-			console.log(result);
+			const result: Settings = await invoke('read_settings');
+			console.log(result.logsFolderLocation);
+			logsFolder.set(result.logsFolderLocation);
 		} catch (error) {
 			console.error('Error querying database:', error);
 		}
 	}
 
 	onMount(() => {
-		getSettings();
+		if ($logsFolder && $logsFolder === '') {
+			getSettings();
+		}
+		else {
+			console.log("logs folder non empty")
+		}
 	});
 </script>
 
@@ -35,6 +44,9 @@
 	<p>
 		Please paste your LOA Logs folder path. This can be found at C:\Users\[USER]\AppData\Local\LOA
 		Logs. You can also open LOA Logs -> settings -> database and you can open the folder from there.
+	</p>
+	<p>
+		current logs folder: {$logsFolder}
 	</p>
 	<input
 		bind:value={inputText}
